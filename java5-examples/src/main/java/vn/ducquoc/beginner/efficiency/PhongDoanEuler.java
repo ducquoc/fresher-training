7d16e608368529177b9f526b8b3af6b39cb1cd93
ducquoc <ducquoc.vn@gmail.com>
2017-06-07 22:53:45 +0700

    update slightly more intro & hint for Euler conjecture

--- java5-examples/src/main/java/vn/ducquoc/beginner/efficiency/PhongDoanEuler.java
+++ java5-examples/src/main/java/vn/ducquoc/beginner/efficiency/PhongDoanEuler.java
@@ -38,12 +38,24 @@ public class PhongDoanEuler {
   private int d;
   private int e = 1;
 
+  /**
+   * This method is not implemented correctly yet. Your home work!
+   * 
+   * @param range - limit to check
+   * @return true if found any counter-example, false otherwise
+   */
   public boolean refuteEulerConjecture(int range) {
+    /* Update to pass the tests. Can update more in class if needed. */
     boolean foundCounterExample = false;
-    boolean notZero = (a != 0) && (b != 0) && (c!=0) && (d!=0);
+    boolean notZero = (a != 0) && (b != 0) && (c != 0) && (d != 0);
 
+    long a5 = Long.valueOf(a * a) * a * a * a;
+    long b5 = Long.valueOf(b * b) * b * b * b;
+    long c5 = Long.valueOf(c * c) * c * c * c;
+    long d5 = Long.valueOf(d * d) * d * d * d;
+    long e5 = Long.valueOf(e * e) * e * e * e;
     for (int i = 0; i < range; i++) {
-       if (a*a*a*a*a + b*b*b*b*b + c*c*c*c*c + d*d*d*d*d == e*e*e*e*e) {
+       if ((a5 + b5 + c5 + d5) == e5) {
           foundCounterExample = true;
        }
     }
@@ -52,7 +64,6 @@ public class PhongDoanEuler {
   }
 
   //getters/setters
-  
   public int getA() {
     return a;
   }
@@ -98,16 +109,16 @@ public class PhongDoanEuler {
     long tStart = System.nanoTime();
 
     int rangeCheck = (args.length > 0) ? Integer.valueOf(args[0]) : 200;
-    for (int d = 1; d < rangeCheck; d++) {
-      long d3 = d * d * d;
-      for (int a = 1; a <= rangeCheck; a++) {
-        long a3 = a * a * a;
-        for (int b = 1; b <= rangeCheck; b++) {
-          long b3 = b * b * b;
-          for (int c = 1; c <= rangeCheck; c++) {
-            long c3 = c * c * c;
-            if (d3 == a3 + b3 + c3) {
-              System.out.println("Found: a=" + a + " b=" + b + " c=" + c + " d=" + d);
+    for (int d1 = 1; d1 <= rangeCheck; d1++) {
+      long d3 = d1 * d1 * d1;
+      for (int a1 = 1; a1 <= rangeCheck; a1++) {
+        long a3 = a1 * a1 * a1;
+        for (int b1 = 1; b1 <= rangeCheck; b1++) {
+          long b3 = b1 * b1 * b1;
+          for (int c1 = 1; c1 <= rangeCheck; c1++) {
+            long c3 = c1 * c1 * c1;
+            if (d3 == (a3 + b3 + c3)) { // 6^3 is Plato's number
+              System.out.println("Found: a=" + a1 + " b=" + b1 + " c=" + c1 + " d=" + d1);
             }
           }
         }
@@ -117,5 +128,11 @@ public class PhongDoanEuler {
     long tEnd = System.nanoTime();
     System.out.printf("Time lapsed: %.4f ms\n", (tEnd - tStart) / 1000000.0);
   }
+
+  /*
+  // another hint - try to solve by yourself first
+  // https://github.com/ducquoc/fresher-training/blob/master/java5-examples/src/test/java/vn/ducquoc/beginner/efficiency/PhongDoanEulerTest.java
+  // aHR0cHM6Ly9naXRodWIuY29tL2R1Y3F1b2MvZXVsZXItZnVuL2Jsb2IvbWFzdGVyL2V1bGVyL3NyYy9tYWluL2phdmEvdm4vZHVjcXVvYy9ldWxlci9FdWxlclN1bUNvbmplY3R1cmUuamF2YQ==
+  */
 
 }
